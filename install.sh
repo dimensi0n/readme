1017e93f7ba10b2a000c69d9fe695535783887c3
Erwan ROUSSEL <erwan.roussel51@gmail.com>
2019-03-27 14:06:26 +0100

    Replace old link

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo curl https://raw.githubusercontent.com/tj/mad/master/bin/mad -o /bin/mad
 #---
 ## Curl the file and move it to /bin
 #---
-sudo curl https://raw.githubusercontent.com/dimensi0n/gitman/master/readme -o /bin/readme
+sudo curl https://raw.githubusercontent.com/dimensi0n/readme/master/readme -o /bin/readme
 
 #---
 ## Add permission
